ed319dea5d4343e335dec88b984999f173062c3a
daminim2004 <brijendratiwari993@gmail.com>
2025-04-07 23:43:05 +0530

    Path dependecies aded

--- Server.js
+++ Server.js
@@ -3,6 +3,7 @@ const mysql = require("mysql2");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
+const path = require("path");
 const cookieParser = require("cookie-parser");
 const verifyToken = require("./verifyUser");
 //
